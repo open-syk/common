54ced59eb4cd2d5990d7d4dea57c48bbc1d84c3a
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-03-13 17:48:58 -0500

    feat: Implemented Error Validator in @syk/common/errors #2

---
+++ src/errors/index.ts
@@ -0,0 +1,16 @@
+export function orThrowError(errorType: any) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor,
+  ) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (...args: any[]) {
+      if (!args[0]) {
+        throw new errorType(args[1]);
+      }
+      return originalMethod.apply(this, args);
+    };
+    return descriptor;
+  };
+}
