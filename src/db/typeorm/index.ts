2a9a72895baba6ff0b55e5c4a5d22e04e708ab43
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-03-14 20:30:47 -0500

    feat: Implemented module typeorm with dataSource, onSession and onTransaction for @open-syk/common/nestjs/typeorm #3

---
+++ src/db/typeorm/index.ts
@@ -0,0 +1,47 @@
+import { DataSource, EntityManager } from 'typeorm';
+
+export interface TypeORMSession {
+  onSession: <T>(
+    dataSource: DataSource,
+    callback: (manager: EntityManager) => Promise<T>,
+  ) => Promise<T>;
+
+  onTransaction: <T>(
+    dataSource: DataSource,
+    callback: (manager: EntityManager) => Promise<T>,
+  ) => Promise<T>;
+}
+
+export const onSession = async <T>(
+  dataSource: DataSource,
+  callback: (manager: EntityManager) => Promise<T>,
+): Promise<T> => {
+  const queryRunner = dataSource.createQueryRunner();
+  try {
+    await queryRunner.connect();
+    const response = await callback(queryRunner.manager);
+    return response;
+  } catch (error) {
+    throw error;
+  } finally {
+    await queryRunner.release();
+  }
+};
+
+export const onTransaction = async <T>(
+  dataSource: DataSource,
+  callback: (manager: EntityManager) => Promise<T>,
+): Promise<T> => {
+  const runner = await dataSource.createQueryRunner();
+  await runner.connect();
+  await runner.startTransaction();
+  try {
+    const response = await callback(runner.manager);
+    await runner.commitTransaction();
+    return response;
+  } catch (err) {
+    await runner.rollbackTransaction();
+  } finally {
+    await runner.release();
+  }
+};
