9f310a5e17c017c0a82fa1992cbc7f0dc88c4d48
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-03-19 20:53:43 -0500

    feat: Add config for read virtual enviroments @open-syk/config #24

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+import { existsSync } from 'fs';
+import nconf from 'nconf';
+import * as path from 'path';
+
+export function getEnvPath(dest: string): string {
+  const env: string | undefined = process.env.NODE_ENV;
+  const fallback: string = path.resolve(`${dest}/.env`);
+  const filename: string = env ? `${env}.env` : 'development.env';
+  let filePath: string = path.resolve(`${dest}/${filename}`);
+  if (!existsSync(filePath)) {
+    filePath = fallback;
+  }
+  return filePath;
+}
+
+export default class SykConfigSingleton {
+  private wasCompleted: boolean;
+  private data: any;
+  private static instance: SykConfigSingleton;
+
+  private constructor() {
+    this.wasCompleted = false;
+  }
+
+  public static getInstance(): SykConfigSingleton {
+    if (!SykConfigSingleton.instance) {
+      SykConfigSingleton.instance = new SykConfigSingleton();
+    }
+    return SykConfigSingleton.instance;
+  }
+
+  public run(pathFile: string): any {
+    if (this.wasCompleted) {
+      return this.data;
+    }
+    this.wasCompleted = true;
+    const pathFileEnv = getEnvPath(pathFile);
+    nconf.argv().env({ separator: '__', parseValues: true, lowerCase: false });
+    nconf.defaults({ conf: path.resolve(pathFileEnv) });
+    nconf.file(nconf.get('conf'));
+    this.data = nconf;
+    return this.data;
+  }
+}
