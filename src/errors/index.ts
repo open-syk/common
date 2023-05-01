ea7cd423de9c3f9381e78795d00f593af411c87f
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-05-01 17:13:01 -0500

    [CHORE] update version to 0.3.0

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -8,9 +8,7 @@ export function orThrowError(errorType: any) {
     const originalMethod = descriptor.value;
     descriptor.value = function (...args: any[]) {
       if (args.length !== 2) {
-        throw new Error(
-          'Exactly 2 arguments are required. response: any, messageError: string',
-        );
+        throw new Error('Exactly 2 arguments are required. (response: any, messageError: string)');
       }
       if (!args[0]) {
         throw new errorType(args[1]);
