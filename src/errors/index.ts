53e5dbdf989fa72cb546c5837731758948e2ec52
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-05-01 17:05:57 -0500

    [FEATURE] COMMON-71: Add validations to orThrowError decorator

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -7,6 +7,11 @@ export function orThrowError(errorType: any) {
   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     descriptor.value = function (...args: any[]) {
+      if (args.length !== 2) {
+        throw new Error(
+          'Exactly 2 arguments are required. response: any, messageError: string',
+        );
+      }
       if (!args[0]) {
         throw new errorType(args[1]);
       }
