c9f702754eaf995c07523dc0b699c0bc8ab07fa7
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-07-08 18:31:12 -0500

    [FEATURE] Add postgresql feature 2/n

--- src/nestjs/db/postgresql/postgresql.module.ts
+++ src/nestjs/db/postgresql/postgresql.module.ts
@@ -1,30 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 
-import postgresql, { Session } from '../../../db/postgresql';
+import { PgGateway } from './pg.service';
 
-export type PSQLSession = Session;
-
-const entityPostgreSQL = postgresql('postgresql');
-
-@Injectable()
-export class PgGateway {
-  public async newSession(): Promise<PSQLSession> {
-    return entityPostgreSQL.newSession();
-  }
-
-  public closeSession(session: PSQLSession): void {
-    return entityPostgreSQL.closeSession(session);
-  }
-
-  public async onSession<T>(operation: (session: PSQLSession) => Promise<T>): Promise<T> {
-    return entityPostgreSQL.onSession<T>(operation);
-  }
-
-  public async onTransaction<T>(operation: (session: PSQLSession) => Promise<T>): Promise<T> {
-    return entityPostgreSQL.onTransaction<T>(operation);
-  }
-
-  public async closePool(): Promise<void> {
-    return entityPostgreSQL.closePool();
+@Module({})
+export class PostgreSQLModule {
+  static forRoot(): DynamicModule {
+    return {
+      module: PostgreSQLModule,
+      providers: [PgGateway],
+      exports: [PgGateway],
+    };
   }
 }
